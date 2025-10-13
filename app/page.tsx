2de7ac43cbf436edf96e3b3af42f24148c8573a8
kenji-arata <knjart1226@gmail.com>
2025-10-13 15:30:28 +0900

    fetch record sample

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
+import { neon } from "@neondatabase/serverless";
+import React from "react";
+
+const fetchComments = async () => {
+  try {
+    const sql = neon(process.env.DATABASE_URL!);
+    const result = await sql`SELECT * FROM test_comments`;
+    const recordList = Array.from(result);
+    return recordList;
+  } catch (error) {
+    console.error("Database error:", error);
+    return [];
+  }
+};
+
 const Home = () => {
-  return <>hoge</>;
+  const comments = React.use(fetchComments());
+  console.log(comments);
+
+  return <>{comments.map((comment) => comment.comment)}</>;
 };
 
 export default Home;
